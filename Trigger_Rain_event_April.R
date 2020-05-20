e37307ac95d5966576f2c616662863b13ce1c2a3
Ouzman <ouz001@csiro.au>
2020-05-20 17:18:01 +1000

    most of the think is done to lookup the data

--- Trigger_Rain_event_April.R
+++ Trigger_Rain_event_April.R
@@ -24,6 +24,8 @@ load.libraries(libs)
 #### Bring in site data
 lameroo <- read.csv("W:/Pastures/Gridded_seasonal_break/Check_code_selected_sites/Lameroo_seasonal_break_yrs.csv")
 
+
+
 lameroo_look_up <- gather(lameroo,
                       year, day_of_year,
                       Year_1971:Year_2018)
@@ -52,17 +54,16 @@ getwd()
 #------------------------------------------------------------------------------------------------------------------
 #bring in my spatial data
 
+#set the area for running the analysis
+#site_import <- st_read("W:/Pastures/Gridded_seasonal_break/Boundary_for_analysis/Lamaroo_rectangle.shp")
+site_import <- st_read("W:/Pastures/Gridded_seasonal_break/Boundary_for_analysis/GRDC_AgroEcological_zones_boundaries_06_region_jax.shp")
 
-
-site_import <- st_read("W:/Pastures/Gridded_seasonal_break/Boundary_for_analysis/Lamaroo_rectangle.shp")
 
 site_sf <- as(site_import, "Spatial") #convert to a sp object
-year_input <- 1972
-site_name <- "Lameroo"
+site_name <- "Aust"
 site <- site_sf
 plot(site)
-rolling_avearge_days = 5
- 
+
 #------------------------------------------------------------------------------------------------------------
 ##1. define the boundary with and use a single layer raster 
 
@@ -86,6 +87,16 @@ site_bound_pts_df <- dplyr::select(site_bound_pts_df, x, y)
 site_bound_pts_df_point <- SpatialPointsDataFrame(site_bound_pts_df[,c("x", "y")], site_bound_pts_df)
 
 head(site_bound_pts_df_point)
+
+
+#-----------------
+
+site_sf <- as(site_import, "Spatial") #convert to a sp object
+year_input <- 1975
+site_name <- "Aust"
+site <- site_sf
+plot(site)
+rolling_avearge_days = 5
 
 
 daily_rain <- brick(
@@ -130,23 +141,130 @@ names(Rain_extract_wide) <- c("POINT_X", "POINT_Y",
                                    "199", "200", "201", "202", "203", "204", "205", "206", "207", 
                                    "208", "209", "210", "211", "212", "213")
 #Remove the clm that have no data for  Rain_evap and add the coords
-str(Rain_extract_wide)
-tail(Rain_extract_wide)
+#str(Rain_extract_wide)
+#tail(Rain_extract_wide)
 Rain_extract_wide <- dplyr::select(Rain_extract_wide, -"61", -"62", -"63", -"64", -"65", -"66" )
 Rain_extract_wide_x_y <- dplyr::select(Rain_extract_wide, "POINT_X",  "POINT_Y")
 Rain_extract_wide_values <- dplyr::select(Rain_extract_wide,"67":"213")
 #make a df with cood and values also add a clm that has a unquie id for grid cell
 Rain_extract_df <- cbind(Rain_extract_wide_x_y, Rain_extract_wide_values)
 Rain_extract_df <- mutate(Rain_extract_df, x_y = paste0(POINT_X, "_", POINT_Y))
+Rain_extract_df
 Rain_extract_df_narrow <- gather(Rain_extract_df, 
                                       key = "day", value = "Rain", `67`:`213` )
 head(Rain_extract_df_narrow) # this is only for one year and one site
 
 #for the day clm I want to look up the rain value
 head(Rain_extract_df_narrow) #1972
-head(lameroo_look_up )
+# Rename the clm called Rain to match the year eg Rain_Yr
+Rain_extract_df_narrow <- rename(Rain_extract_df_narrow, c("Rain"= paste0("Rain_", year_input)))
+
+head(Rain_extract_df_narrow ) #this is all of Aust rainfall for ach day but just one year
+
+### Lets try and build this up for a few years
+
+#Rain_extract_df_narrow_1972_1975 <- Rain_extract_df_narrow
+Rain_extract_df_narrow_1972_1975 <- left_join(Rain_extract_df_narrow_1972_1975, Rain_extract_df_narrow)
+head(Rain_extract_df_narrow_1972_1975 )
+
+
+
+
+#subset my data for x_y =  114_-27.15
+
+subset_Rain_extract_df_narrow_1972_1975 <- filter(Rain_extract_df_narrow_1972_1975, x_y == "146.1_-30.7")
+head(subset_Rain_extract_df_narrow_1972_1975)
+
+# I want a list of ID numbers for the sites I am interested in
+
+### Bring in the data that I want to look up.
+seasonal_break_output <-read.csv("W:/Pastures/Gridded_seasonal_break/Check_code_selected_sites/GRDC_zone_seasonal_break_yrs_v3_join_study_sites.csv")
+
+subset_seasonal_break_output <- filter(seasonal_break_output, x_y == "146.1_-30.7")
+head(subset_seasonal_break_output)
+
+#make this look better....narrow dataset
+subset_seasonal_break_output <- gather(subset_seasonal_break_output, 
+                                 key = "Year", value = "day", 'Year_1971':'Year_2018' )
+head(subset_seasonal_break_output)
+subset_seasonal_break_output <- separate(subset_seasonal_break_output,Year, c("junk", "year"), "_" )
+subset_seasonal_break_output <- dplyr::select(subset_seasonal_break_output, year, day)
+
+
+
+head(subset_seasonal_break_output) #df
+head(subset_Rain_extract_df_narrow_1972_1975) #lookup
+
+
+rain_long <- gather(subset_Rain_extract_df_narrow_1972_1975, 
+                      key = "year", value = "rain", "Rain_1972": "Rain_1975" ) # this is the range of years
+
+head(rain_long)
+#strip the rain out of the name
+rain_long <- separate(rain_long,year, c("junk", "year"), "_" )
+rain_long$year <- as.integer(rain_long$year)
+subset_seasonal_break_output$year <- as.integer(subset_seasonal_break_output$year)
+
+day_break_rain <- subset_seasonal_break_output %>%
+  left_join(rain_long, by = c("day","year"))
+
+head(day_break_rain)
+
+
+
+
+
+
+
+
+str(subset_seasonal_break_output)
+str(subset_Rain_extract_df_narrow_1972_1975)
+subset_Rain_extract_df_narrow_1972_1975$day <- as.numeric(subset_Rain_extract_df_narrow_1972_1975$day )
+#I want to add a clm to subset_seasonal_break_output
+
+#Can I join it???
+test <- left_join(subset_seasonal_break_output, subset_Rain_extract_df_narrow_1972_1975)
+head(test)
+
+
+test <- mutate(test, look_up_clm = paste0("Rain_", year))
+
+
+
+
+
+filter(subset_seasonal_break_output, day %in% rain_in_year_I_want) #this pulls out value of 186 
+test2 <- 1973
+dplyr::select(test, paste0("Rain_", test2))
+
+test3 <- mutate(test, report_rain = (dplyr::select(test, paste0("Rain_", test2))))
+test3 <- mutate(test, report_rain = Rain_1973)
+head(test3)
+
+# test3 <- test %>% rowwise() %>% mutate(new_clm = min(Rain_1972,Rain_1975))
+# head(test3)
+
+head(test)
+
+test %>%
+  mutate(new_clm = ifelse(day == "1971", "XXXX", ColB))
+
+
+
+
+
+#for lamaroo this might be 93953 (i can't remember how I did this???)
+
+
+
+
+
 year_I_want <- filter(lameroo_look_up, year_numb == "1972")
 year_I_want <- year_I_want[1,2]
 year_I_want
 rain_in_year_I_want <- filter(Rain_extract_df_narrow, day == year_I_want)
 rain_in_year_I_want
+
+
+
+filter(Rain_extract_df_narrow, day %in% rain_in_year_I_want) #this pulls out value of 186 
